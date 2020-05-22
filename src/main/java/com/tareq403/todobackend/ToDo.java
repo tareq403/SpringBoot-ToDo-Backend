c57dce563e5c348771c4aafd46eb6f4601f2d0b2
Tareq <alnoortareq4@gmail.com>
2020-05-23 00:31:33 +0600

    Prepared ToDo model

---
+++ src/main/java/com/tareq403/todobackend/ToDo.java
@@ -0,0 +1,34 @@
+package com.tareq403.todobackend;
+
+import org.springframework.data.annotation.Id;
+
+public class ToDo {
+    @Id
+    private String id;
+    private String name;
+    private boolean done;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
